91343d99a9d68211c2083cee3d19126ef6d10506
Kieran Griffiths <griffk04@gmail.com>
2022-02-19 10:29:33 +0800

    Cleaned up position_translation function

--- src/bin/snake.rs
+++ src/bin/snake.rs
@@ -134,18 +134,16 @@ fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
 }
 
 fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
-    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
-        let tile_size = bound_window / bound_game;
-        pos / bound_game * bound_window - (bound_window / 2.0) + (tile_size / 2.0)
+    fn convert(pos: i32, window: f32, game: u32) -> f32 {
+        let tile_size = window / game as f32;
+        pos as f32 / game as f32 * window - (window / 2.0) + (tile_size / 2.0)
     }
 
     let window = windows.get_primary().unwrap();
     for (pos, mut transform) in q.iter_mut() {
-        transform.translation = Vec3::new(
-            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
-            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
-            0.0,
-        )
+        let x = convert(pos.x, window.width(), ARENA_WIDTH);
+        let y = convert(pos.y, window.height(), ARENA_HEIGHT);
+        transform.translation = Vec3::new(x, y, 0.0)
     }
 }
 
